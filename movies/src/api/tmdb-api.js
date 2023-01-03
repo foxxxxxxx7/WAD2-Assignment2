c9994eacd0032d9744f0e1660c37dad2694b300a
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2023-01-03 23:02:25 +0000

    movies-api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -204,3 +204,23 @@ export const getTopTV = (page) => {
             throw error
         });
 };
+
+export const login = (username, password) => {
+    return fetch('/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
+
+export const signup = (username, password) => {
+    return fetch('/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
