ecaf468d85ebdd796f59263077a68e7f44589c01
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2023-01-08 20:59:53 +0000

    update RemoveFromFavborites

--- movies/src/components/cardIcons/removeFromTVFavorites.js
+++ movies/src/components/cardIcons/removeFromTVFavorites.js
@@ -2,13 +2,17 @@ import React, { useContext } from "react";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import {TVContext } from "../../contexts/TVContext";
+import { AuthContext } from "../../contexts/authContext";
 
 const RemoveFromTVFavoritesIcon = ({ tv }) => {
+    const contextAuth = useContext(AuthContext);
     const context = useContext(TVContext);
+    const user = contextAuth.userEmail
+
 
     const handleRemoveFromTVFavorites = (e) => {
         e.preventDefault();
-        context.removeFromTVFavorites(tv);
+        context.removeFromTVFavorites(user, tv);
     };
     return (
         <IconButton
