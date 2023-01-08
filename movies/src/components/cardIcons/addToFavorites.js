22ad26138c17d96ea76042f9bdc46b416a816f7b
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2023-01-08 20:19:39 +0000

    update add to Favorites

--- movies/src/components/cardIcons/addToFavorites.js
+++ movies/src/components/cardIcons/addToFavorites.js
@@ -2,14 +2,24 @@ import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { AuthContext } from "../../contexts/authContext";
+import { useNavigate } from "react-router-dom";
+
 
 const AddToFavoritesIcon = ({ movie }) => {
+    const contextAuth = useContext(AuthContext);
     const context = useContext(MoviesContext);
+    const navigate = useNavigate();
 
     const handleAddToFavorites = (e) => {
+        //if (loading) return;
+        if (!contextAuth.isAuthenticated) {
+          return navigate("/")
+        }
         e.preventDefault();
-        context.addToFavorites(movie);
-    };
+        //context.addToFavorites(movie);
+        context.addToFavorites(contextAuth.userEmail, movie.id);
+      };
 
     return (
         <IconButton aria-label="add to favorites" onClick={handleAddToFavorites}>
--- movies/src/components/cardIcons/addToTVFavorites.js
+++ movies/src/components/cardIcons/addToTVFavorites.js
@@ -2,14 +2,25 @@ import React, { useContext } from "react";
 import { TVContext } from "../../contexts/TVContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteTVIcon from "@mui/icons-material/Favorite";
+import { AuthContext } from "../../contexts/authContext";
+import { useNavigate } from "react-router-dom";
+
 
 const AddToTVFavoritesIcon = ({ tv }) => {
+    const contextAuth = useContext(AuthContext);
     const context = useContext(TVContext);
+    const navigate = useNavigate();
 
     const handleAddToTVFavorites = (e) => {
+        //if (loading) return;
+        if (!contextAuth.isAuthenticated) {
+          return navigate("/")
+        }
         e.preventDefault();
-        context.addToTVFavorites(tv);
-    };
+        //context.addToFavorites(movie);
+        context.addToTVFavorites(contextAuth.userEmail, tv.id);
+      };
+
 
     return (
         <IconButton aria-label="add to favorites" onClick={handleAddToTVFavorites}>
